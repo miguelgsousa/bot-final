ac2eef4d7266179aa3ef0962d7db018fbb29eb5d
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-03-08 10:54:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,8 +44,8 @@ async def on_message(message):
         s = d.seconds * 1000 + d.microseconds // 1000
         await client.send_message(message.channel, ':ping_pong: Pong! {}ms'.format(s))
     
-    if message.content.lower().startswith('/diz'):
-        mensagem = re.sub('/diz ', '', message.content)
+    if message.content.lower().startswith('!diz'):
+        mensagem = re.sub('!diz ', '', message.content)
         canaltxt = mensagem.split(' ', 1)
         canaltxt = str(canaltxt[0])
         mensagem = re.sub(canaltxt, '', mensagem)

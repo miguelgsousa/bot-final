3d01ae488fd55167ac53a6cea051120596a758a5
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-03-08 10:57:29 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,6 @@ async def on_message(message):
         canaltxt = str(canaltxt[0])
         mensagem = re.sub(canaltxt, '', mensagem)
         canal = discord.utils.find(lambda c: c.name == canaltxt or c.id == canaltxt or c.mention == canaltxt, message.author.server.channels)
-        await client.send_message(canal, mensagem
-        
-        
+        await client.send_message(canal, mensagem)
+         
 client.run(token)

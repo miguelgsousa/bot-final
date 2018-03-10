55c82aa14f35a1348ff77662f1e74b28a55e2590
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-03-10 11:23:30 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def on_member_join(member):
 @client.event
 async def on_member_ban(user):
     channel = discord.utils.find(lambda c: c.name == 'general', user.server.channels)
-    embed = discord.Embed(title="** AI AI AI LIKE THIS YOU KILL DADDY!**", description="the member **@{0.name}** was banned from the server".format(user), color=0xFF7F00
+    embed = discord.Embed(title="**LIKE THIS YOU KILL DADDY!**", description="the member **@{0.name}** was banned from the server".format(user), color=0xFF7F00
 
 )
     embed.set_thumbnail(url=user.avatar_url)

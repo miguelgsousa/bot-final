e3fa7403dd386b814f1eaa01542919b0eeddc8d0
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-03-28 13:41:32 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import re
 import aiohttp
 from datetime import datetime, timedelta
 import os
+from twitch import TwitchClient
 
 client = discord.Client()
 
@@ -55,5 +56,26 @@ async def on_message(message):
 
     if len(message.content) > 325:
         await client.delete_message(message)
-            
+     
+
+@client.event
+async def on_member_update(memberBEF,memberAFT):
+    if len(memberBEF.roles) != 1:
+        for cargo in memberBEF.roles:
+            if (cargo.name == "Master" and memberAFT.game.type == 1):
+                client = TwitchClient(client_id = twitchClientID)
+                channel = client.channels.get_by_id(twitchChannelID)
+                streamTitle = channel.status
+                gameTitle = channel.game
+
+                print('%s Goularte started streaming. Announced with @everyone in channel %s.' % (time_now(), default_twitch_ann_channel))
+
+                em = discter(text = "Se não quiser mais receber notificações sobre as streams do Goularte, clique no sino em cima dessa página para mutar esse canal, assim: http://bit.ly/mutarcanal")
+                em.set_thumord.Embed(description = 'http://twitch.tv/g0ularte', colour=0x4B367C)
+                em.set_foobnail(url = "https://static-cdn.jtvnw.net/jtv_user_pictures/6b4206db-8f52-4662-a64f-e2098a9b8244-profile_image-70x70.png")
+                em.set_author(name="Goularte está live na Twitch!", url="http://twitch.tv/g0ularte", icon_url=memberAFT.avatar_url)
+                em.add_field(name = "Jogando", value = gameTitle, inline = True)
+                em.add_field(name = "Título da Stream", value = streamTitle, inline = True)
+                await client.send_message(client.get_channel(default_twitch_ann_channel), '@everyone', embed = em)    
+    
 client.run(token)

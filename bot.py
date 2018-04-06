b3821c183b8cb916f66fdd91c5a7107b803e299b
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-04-05 22:46:43 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,29 +15,35 @@ if is_prod:
     token = os.environ.get('TOKEN')
 else:
     import secreto
+
     token = secreto.token
+
 
 @client.event
 async def on_ready():
     await client.change_presence(
         game=discord.Game(type=0, name='with style'))
 
+
 @client.event
 async def on_member_join(member):
     await client.send_message(member,
-                           '-  Helo **'+ member.name +'** welcome to **'+ member.server.name +'** !  - \n First read the rules in # welcome, if you want to call a person or a friend to serve you use this link:https://discord.gg/eRjQKEJ \n have a good time :wink:')
-    
+                              '-  Helo **' + member.name + '** welcome to **' + member.server.name + '** !  - \n First read the rules in # welcome, if you want to call a person or a friend to serve you use this link:https://discord.gg/eRjQKEJ \n have a good time :wink:')
+
     await client.add_roles(member)
+
 
 @client.event
 async def on_member_ban(user):
     channel = discord.utils.find(lambda c: c.name == 'general', user.server.channels)
-    embed = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ", description="the member **@{0.name}** was banned from the server".format(user), color=0xFF7F00
+    embed = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",
+                          description="the member **@{0.name}** was banned from the server".format(user), color=0xFF7F00
 
-)
+                          )
     embed.set_thumbnail(url=user.avatar_url)
-    await client.send_message(channel, embed=embed)       
- 
+    await client.send_message(channel, embed=embed)
+
+
 @client.event
 async def on_message(message):
     if message.content.lower().startswith('!ping') and not message.author.id == '419133912330272779':
@@ -46,19 +52,29 @@ async def on_message(message):
         await client.send_message(message.channel, ':ping_pong: Pong! {}ms'.format(s))
 
     if message.content.lower().startswith('!diz'):
-       mensagem = re.sub('!diz ', '', message.content)
-       canaltxt = mensagem.split(' ', 1)
-       canaltxt = str(canaltxt[0])
-       mensagem = re.sub(canaltxt, '', mensagem)
-       canal = discord.utils.find(lambda c: c.name == canaltxt or c.id == canaltxt or c.mention == canaltxt, message.author.server.channels)
-       await client.send_message(canal, mensagem)
+        mensagem = re.sub('!diz ', '', message.content)
+        canaltxt = mensagem.split(' ', 1)
+        canaltxt = str(canaltxt[0])
+        mensagem = re.sub(canaltxt, '', mensagem)
+        canal = discord.utils.find(lambda c: c.name == canaltxt or c.id == canaltxt or c.mention == canaltxt,
+                                   message.author.server.channels)
+        await client.send_message(canal, mensagem)
 
     if len(message.content) > 325:
-        await client.delete_message(message)     
-        
+        await client.delete_message(message)
+
 @client.event
-async def on_message(message):   
+async def on_message(message):
     if message.channel == client.get_channel('414445398212476928'):
         await client.add_reaction(message, "😐")
-                            
+    if message.channel == client.get_channel('414449863812710400'):
+        await client.add_reaction(message, "😐")
+    if message.author.id == ('419133912330272779'):
+        await client.add_reaction(message, "😐")
+    if message.author.id == ('159985870458322944'):
+        await client.add_reaction(message, "😐")
+    if message.author.id == ('155149108183695360'):
+        await client.add_reaction(message, "😐")
+
+
 client.run(token)

0c26fd651eb9970d53a7f083df1bd19f11f59800
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-04-05 21:16:37 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,6 +38,20 @@ async def on_member_ban(user):
     embed.set_thumbnail(url=user.avatar_url)
     await client.send_message(channel, embed=embed)   
     
+
+@client.event
+async def on_message(message):
+    if message.channel == client.get_channel('414445398212476928'):
+        await client.add_reaction(message, "😐")
+    if message.channel == client.get_channel('414449863812710400'):
+        await client.add_reaction(message, "😐")
+    if message.author.id == ('419133912330272779'):
+        await client.add_reaction(message, "😐")
+    if message.author.id == ('159985870458322944'):
+        await client.add_reaction(message, "😐")
+    if message.author.id == ('155149108183695360'):
+        await client.add_reaction(message, "😐")    
+    
 @client.event
 async def on_message(message):
     if message.content.lower().startswith('!ping') and not message.author.id == '419133912330272779':

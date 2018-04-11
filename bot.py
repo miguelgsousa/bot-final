0045e7b287f10b2016d959fe70ec47699d312414
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-04-11 16:33:05 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,12 +51,13 @@ async def on_message(message):
         s = d.seconds * 1000 + d.microseconds // 1000
         await client.send_message(message.channel, ':ping_pong: Pong! {}ms'.format(s))
 
-    if message.content.startswith('!fale'):
-        try:
-            mensagem = message.content[6:]
-            await client.send_message(message.channel,mensagem,tts=False)
-        except:
-            await client.send_message(message.channel, "Você precisa escrever algo para eu falar!")
+    if message.content.lower().startswith('!diz'):
+         mensagem = re.sub('!diz ', '', message.content)
+         canaltxt = mensagem.split(' ', 1)
+         canaltxt = str(canaltxt[0])
+         mensagem = re.sub(canaltxt, '', mensagem)
+         canal = discord.utils.find(lambda c: c.name == canaltxt or c.id == canaltxt or c.mention == canaltxt,message.author.server.channels)
+         await client.send_message(canal, mensagem)
 
     if len(message.content) > 325:
         await client.delete_message(message)

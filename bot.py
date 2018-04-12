39e71016c9fc3ae43a0a41d01e3f09d170ce34ec
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-04-12 00:52:12 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,9 +57,7 @@ async def on_message(message):
 
     if message.content.lower().startswith("!ban"):
         await client.delete_message(message)    
-        role = discord.utils.get(message.server.roles, name='Skyton')
-        role = discord.utils.get(message.server.roles, name='Admin')
-        role = discord.utils.get(message.server.roles, name='Moderator')
+        role = discord.utils.get(message.server.roles, name='Skyton''Admin''Moderator')
         author = message.author.mention
         user = message.mentions[0]
         await client.ban(user)

fa7165a47be642c6ec036ed7fea4424042f2350b
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-04-21 20:54:47 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ async def on_message(message):
     if message.author.id == ('155149108183695360'):
         await client.add_reaction(message, "😐")
 
-    msg = message.user.id    
+    msg = message.user   
     if (msg) > 5:
         member = user.id
         Medion_user = discord.utils.get(member.server.roles, name="Medion user")

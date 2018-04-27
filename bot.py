ba1f8d2d7fcdc0f1cd95373379c160fa7f365be3
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-04-27 18:00:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,6 +58,8 @@ async def on_message(message):
         user = message.mentions[0]
         em = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",description="Moderator **{}** banned member **{}** from server".format(author.name, user),color=0xFF7F00)
         em.set_thumbnail(url=user.avatar_url)
+        em = discord.Embed(title='For one or more reasons you have been banned from skyton server!',description='If you think it was an unfair ban between' '[ in this link ](' + "https://goo.gl/kDKqhF" + ')' 'revoke your ban',colour=0xFF7F00)
+        await client.send_message(member, embed=em)
         await client.send_message(message.channel, embed=em)
         await client.ban(user)
     

568502cb152d0e62564fa31d892a71dd19a3479a
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-04-29 19:33:11 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,9 @@ else:
     token = secreto.token
 
 msgban = discord.Embed(title='For one or more reasons you have been banned from skyton server!',description='If you think it was an unfair ban between' '[ in this link ](' + "https://goo.gl/kDKqhF" + ')' 'revoke your ban',colour=0xFF7F00)                            
-    
+
+msgban3 = discord.Embed(title='You have been banned from skyton server!',description='You have been banned automatically banned for violating rule number 9 of the server,enter' '[ this link ](' + "https://goo.gl/kDKqhF" + ')' 'if any mistake has been made',colour=0xFF7F00)                            
+
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(type=3, name='the events on the server'))

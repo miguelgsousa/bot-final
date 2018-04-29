68f772b8508535cb4d637fac681fbde314b0cd15
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-04-29 18:18:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,6 +66,7 @@ msgban2 = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",descript
     if message.content.lower().startswith("https://www.xvideos.com"):
         await client.delete_message(message)
         user = message.author
+        msgban2 = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",description="The member **{}** was automatically banned for violating rule number 9 of the server".format(user),color=0xFF7F00)
         msgchannel = await client.send_message(message.channel, embed=msgban2)
         await client.add_reaction(msgchannel, "😮")
         await client.send_message(user, embed=msgban)
@@ -74,6 +75,7 @@ msgban2 = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",descript
     if message.content.lower().startswith("http://www.redtube.com"):
         await client.delete_message(message)
         user = message.author
+        msgban2 = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",description="The member **{}** was automatically banned for violating rule number 9 of the server".format(user),color=0xFF7F00)
         msgchannel = await client.send_message(message.channel, embed=msgban2)
         await client.add_reaction(msgchannel, "😮")
         await client.send_message(user, embed=msgban)

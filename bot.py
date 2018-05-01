4ab3e28bd7cbda3154d1673ff14da634a00b7196
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-05-01 16:52:42 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,8 @@ msgban = discord.Embed(title='For one or more reasons you have been banned from 
 
 msgban3 = discord.Embed(title='You have been banned from skyton server!',description='You have been banned automatically banned for violating rule number 9 of the server(send content +18 in text chat),enter' '[ this link ](' + "https://goo.gl/kDKqhF" + ')' 'if any mistake has been made',colour=0xFF7F00)                            
 
+logmsg = discord.object.id == '440210099596689424'
+
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(type=3, name='the events on the server'))
@@ -46,7 +48,11 @@ async def on_message(message):
         em = discord.Embed(description=mensagem, colour=0xdb513a)
         em.set_author(name='Notice of moderation!', icon_url=message.author.avatar_url)
         em.set_footer(text='Att.{}, Skyton server admin.'.format(message.author.name))
+        log = discord.Embed(description=mensagem, colour=0xdb513a)
+        log.set_author(name='{} sent a warning!'.format(message.author.name), icon_url=message.author.avatar_url)
+        log.set_footer(text='Att.{}, Skyton server admin.')
         await client.send_message(canal, '@everyone', embed=em)
+        await client.send_message(logmsg, embed=log)        
         
     if message.content.lower().startswith("!ban"):
         await client.delete_message(message)
@@ -54,12 +60,16 @@ async def on_message(message):
         role = discord.utils.get(message.server.roles, name='Ban')
         author = message.author
         user = message.mentions[0]
-        em = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",description="Moderator **{}** banned member **{}** from server".format(author.name, user),color=0xFF7F00)
+        em = discord.Embed(title="THIS WAY YOU'RE GONNA KILL DADDY!",description="Moderator **{}** banned member **{}** from server".format(author.name, user),color=0xFF7F00)
         em.set_thumbnail(url=user.avatar_url)
+        log = discord.Embed(description="Moderator **{}** banned member **{}** from server".format(author.name, user),color=0xFF7F00)
+        log.set_author(name='{} banned a member!'.format(author.name), icon_url=message.author.avatar_url)
+        log.set_footer(text='a')
         await client.send_message(user, embed=msgban)
         msgchannel = await client.send_message(message.channel, embed=em)
         await client.add_reaction(msgchannel, "😮")
         await client.ban(user)
+        await client.send_message(logmsg, embed=log)
     
     if len(message.content) > 325:
         await client.delete_message(message)
@@ -67,20 +77,26 @@ async def on_message(message):
     if message.content.lower().startswith("https://www.xvideos.com"):
         await client.delete_message(message)
         user = message.author
-        msgban2 = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",description="The member **{}** was automatically banned for violating rule number 9 of the server".format(user),color=0xFF7F00)
+        msgban2 = discord.Embed(title="THIS WAY YOU'RE GONNA KILL DADDY!",description="The member **{}** was automatically banned for violating rule number 9 of the server".format(user),color=0xFF7F00)
         msgchannel = await client.send_message(message.channel, embed=msgban2)
+        log = discord.Embed(description="The member **{}** was automatically banned for violating rule number 9 of the server".format(user),color=0xFF7F00)
+        log.set_author(name='{} Was automatically banned!'.format(author.name), icon_url=user.avatar_url)
         await client.add_reaction(msgchannel, "😮")
         await client.send_message(user, embed=msgban3)
         await client.ban(user)
+        await client.send_message(logmsg, embed=log)
         
     if message.content.lower().startswith("http://www.redtube.com"):
         await client.delete_message(message)
         user = message.author
-        msgban2 = discord.Embed(title=" **THIS WAY YOU'RE GONNA KILL DADDY!** ",description="The member **{}** was automatically banned for violating rule number 9 of the server".format(user),color=0xFF7F00)
+        msgban2 = discord.Embed(title="THIS WAY YOU'RE GONNA KILL DADDY!",description="The member **{}** was automatically banned for violating rule number 9 of the server".format(user),color=0xFF7F00)
         msgchannel = await client.send_message(message.channel, embed=msgban2)
+        log = discord.Embed(description="The member **{}** was automatically banned for violating rule number 9 of the server".format(user),color=0xFF7F00)
+        log.set_author(name='{} Was automatically banned!'.format(author.name), icon_url=user.avatar_url)
         await client.add_reaction(msgchannel, "😮")
         await client.send_message(user, embed=msgban3)
         await client.ban(user)    
+        await client.send_message(logmsg, embed=log)
         
     if message.content.lower().startswith("https://discord.gg"):
         await client.delete_message(message)

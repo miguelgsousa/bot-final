54de85a8d1e7861bea8340ef341a225835844c60
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-05-06 15:43:18 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,6 +58,7 @@ async def on_message(message):
         em.set_footer(text='Att.{}, Skyton server admin.'.format(message.author.name))
         log = discord.Embed(description=mensagem, colour=0xdb513a
         log.set_author(name='{} sent a warning!'.format(message.author.name), icon_url=message.author.avatar_url)
+        log.set_footer(text='aaa')                    
         await client.send_message(canal, '@everyone', embed=em)
         await client.send_message(logmsg, embed=log)        
         
@@ -71,7 +72,7 @@ async def on_message(message):
         em.set_thumbnail(url=user.avatar_url)
         log = discord.Embed(description="Moderator **{}** banned member **{}** from server".format(author.name, user),color=0xFF7F00)
         log.set_author(name='{} banned a member!'.format(author.name), icon_url=message.author.avatar_url)
-        log.set_footer(text=' {}:{} ')
+        log.set_footer(text='aaa')
         await client.send_message(user, embed=msgban)
         msgchannel = await client.send_message(message.channel, embed=em)
         await client.add_reaction(msgchannel, "😮")

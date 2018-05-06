3e195e9be72eda182d2c1302ef2761e7cddea468
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-05-06 13:55:32 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,9 +57,11 @@ async def on_message(message):
         em.set_author(name='Notice of moderation!', icon_url=message.author.avatar_url)
         em.set_footer(text='Att.{}, Skyton server admin.'.format(message.author.name))
         log = discord.Embed(description=mensagem, colour=0xdb513a)
-        d = datetime.utcnow() - message.timestamp
+        temp = datetime.datetime.now()
+        h = temp.hour
+        m = temp.minute
         log.set_author(name='{} sent a warning!'.format(message.author.name), icon_url=message.author.avatar_url)
-        log.set_footer(text='{}:{}'.format(d.hour,d.minute))
+        log.set_footer(text='{}:{}'.format(temp.h,temp.m))
         await client.send_message(canal, '@everyone', embed=em)
         await client.send_message(logmsg, embed=log)        
         

6a6ba8a37a3e23435cc0db949de21a12aecd6715
Skyton25 <36994564+Skyton25@users.noreply.github.com>
2018-05-08 14:23:32 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,9 +16,7 @@ if is_prod:
 else:
     import secreto
 
-    token = secreto.token                                                        
-
-temp = datetime.datetime.now()    
+    token = secreto.token                                                          
     
 @client.event
 async def on_ready():
@@ -58,6 +56,7 @@ async def on_message(message):
         em = discord.Embed(description=mensagem, colour=0xdb513a)
         em.set_author(name='Notice of moderation!', icon_url=message.author.avatar_url)
         em.set_footer(text='Att.{}, Skyton server admin.'.format(message.author.name))
+        temp = datetime.datetime.now()
         log = discord.Embed(description=mensagem, colour=0xdb513a)
         log.set_author(name='{} sent a warning!'.format(message.author.name), icon_url=message.author.avatar_url)
         log.set_footer(text='{}'.format(temp))
